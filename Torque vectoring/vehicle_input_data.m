25327934ae3230ae6de7d202a38659a6ddc2f04e
ramharans <ramharan.mech@gmail.com>
2025-04-23 21:14:12 +0530

    Add files via upload

---
+++ Torque vectoring/vehicle_input_data.m
@@ -0,0 +1,16 @@
+m=356
+Izz=120
+c_yf=15714
+c_yr=21429
+l_f=0.873
+l_r=0.717
+VCG=25
+delta=1.04	
+dt=0.001
+%% 
+% The below code to be run after simulink simulation 
+% individually for the models.here x2 and y2 is
+% are the co ordinates for without torque vectoring model
+% x=out.x2_axis;
+% y=out.y2_axis;
+% plot(x,y)
